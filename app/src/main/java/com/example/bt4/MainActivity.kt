c2f53bf4bdb32b93cc84b2d01c6d21bbb0c5d2a8
Phong Hao <phamphonghao1002@gmail.com>
2020-10-02 22:13:40 +0700

    first commit

---
+++ app/src/main/java/com/example/bt4/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.bt4
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private val button: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
